e416514e305d7fd03366f2986bf2e7f84662e696
Ak4zh <agwl.akash@gmail.com>
2021-09-10 01:27:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,50 @@
 sudo apt update
 sudo apt install git -y
 
+############################
+# remove ubuntu annoyances #
+############################
+sudo apt remove apport apport-gtk  # crash report
+
 #####################################
 # install and setup zsh + oh-my-zsh #
 #####################################
-git clone https://github.com/jotyGill/quickz-sh.git
-cd quickz-sh
-./quickz.sh -c
+# git clone https://github.com/jotyGill/quickz-sh.git
+# cd quickz-sh
+# ./quickz.sh -c        # only run with '-c' the first time, running multiple times will duplicate history entries
+
+####################
+# social snap apps #
+####################
+sudo snap install telegram-desktop
+sudo snap install slack --classic
+sudo snap install discord
+sudo snap install brave
+sudo snap install htop
+sudo snap install node --classic
+sudo snap install pycharm-professional --classic
+sudo snap install --classic code
+
+#####################################
+# install docker and socker-compose #
+#####################################
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 ###################################################
 # install gnome-tweaks-tool and set font scalling #
@@ -52,11 +90,11 @@ install_chrome_extension () {
 }
 
 # install brave
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+# sudo apt install apt-transport-https curl
+# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# sudo apt update
+# sudo apt install brave-browser
 
 # create brave extention dir
 sudo mkdir /opt/brave.com/brave/extensions
@@ -73,15 +111,9 @@ install_chrome_extension "jlmpjdjjbgclbocgajdjefcidcncaied" "daily.dev | The Hom
 
 # install Cloudflare Cloud Wrap
 curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo apt-key add -
-echo 'deb http://pkg.cloudflareclient.com/ focal main' | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+echo 'deb http://pkg.cloudflareclient.com/ $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt update
 sudo apt install cloudflare-warp
 warp-cli register
 warp-cli connect
 warp-cli enable-always-on
-
-###########################
-# install pycharm, vscode #
-###########################
-sudo snap install pycharm-professional --classic
-sudo snap install --classic code
